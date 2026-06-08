7671622ae8bc2f91f34ee4a773e26b6f0ab7ea48
prarthana08prarthu <prarthana08prarthu@gmail.com>
2026-06-08 20:02:55 +0530

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -2,6 +2,14 @@
 #define GRAPHICS_H
 
 #include "canvas.h"
+typedef struct {
+    char type; // 'C' circle, 'R' rectangle, 'L' line, 'T' triangle
+    int params[6]; // store coordinates or radius depending on type
+} Shape;
+
+Shape objects[100];
+int object_count;
+
 
 // Drawing functions
 void draw_line(Canvas* canvas, int x1, int y1, int x2, int y2);
@@ -12,5 +20,12 @@ void draw_triangle(Canvas* canvas, int x1, int y1, int x2, int y2, int x3, int y
 // Helper functions
 void bresenham_line(Canvas* canvas, int x1, int y1, int x2, int y2);
 void midpoint_circle(Canvas* canvas, int cx, int cy, int radius);
+objects[object_count].type = 'L';
+objects[object_count].params[0] = x1;
+objects[object_count].params[1] = y1;
+objects[object_count].params[2] = x2;
+objects[object_count].params[3] = y2;
+object_count++;
+
 
 #endif
